820edacfc08f1d1d41d67ee752a72fa09799a2b8
alphonse37 <jesus.colocho.37@gmail.com>
2019-08-10 23:36:23 -0600

    tarjetas y slider mejorado

--- js/script.js
+++ js/script.js
@@ -13,3 +13,20 @@ $(document).ready(function() {
 $('.container').on('click', function () {
     $('.card1').toggleClass('flipped');
   });
+
+
+(function () {
+    var options = {
+        whatsapp: "+ (651) 443-6926", // WhatsApp number
+        email: "contact@mmpaintingmn.com", // Email
+        call_to_action: "¡Contact us!", // Call to action
+        button_color: "#E74339", // Color of button
+        position: "right", // Position may be 'right' or 'left'
+        order: "whatsapp,email", // Order of buttons
+    };
+    var proto = document.location.protocol, host = "whatshelp.io", url = proto + "//static." + host;
+    var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = url + '/widget-send-button/js/init.js';
+    s.onload = function () { WhWidgetSendButton.init(host, proto, options); };
+    var x = document.getElementsByTagName('script')[0]; x.parentNode.insertBefore(s, x);
+})();
+
